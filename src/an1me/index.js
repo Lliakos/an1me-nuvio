2c0a2cc645912cbd51939e9d0abf616c0062a0dd
Lliakos <philip_k06@yahoo.com>
2026-06-04 16:07:42 +0300

    Fixes!!!!

--- src/an1me/index.js
+++ src/an1me/index.js
@@ -1,4 +1,15 @@
+const { getPage } = require('./http.js');
+const { extract } = require('./extractor.js');
+
 async function getStreams(tmdbId, mediaType, season, episode) {
-  return [{ title: "TEST", url: "https://google.com", headers: {} }];
+  try {
+    const html = await getPage(tmdbId, mediaType, season, episode);
+    // This now returns the real streams from your extractor
+    return extract(html); 
+  } catch (e) {
+    console.error("Provider Error:", e);
+    return [];
+  }
 }
+
 module.exports = { getStreams };
