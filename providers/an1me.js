5259b6a11ee9f2ec2427f5acce760453541c9594
Lliakos <philip_k06@yahoo.com>
2026-06-04 16:25:21 +0300

    Fixes!!!!

--- providers/an1me.js
+++ providers/an1me.js
@@ -1,98 +1,11 @@
-var __getOwnPropNames = Object.getOwnPropertyNames;
-var __commonJS = (cb, mod) => function __require() {
-  return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
-};
-var __async = (__this, __arguments, generator) => {
-  return new Promise((resolve, reject) => {
-    var fulfilled = (value) => {
-      try {
-        step(generator.next(value));
-      } catch (e) {
-        reject(e);
-      }
-    };
-    var rejected = (value) => {
-      try {
-        step(generator.throw(value));
-      } catch (e) {
-        reject(e);
-      }
-    };
-    var step = (x) => x.done ? resolve(x.value) : Promise.resolve(x.value).then(fulfilled, rejected);
-    step((generator = generator.apply(__this, __arguments)).next());
-  });
-};
-
-// src/an1me/http.js
-var require_http = __commonJS({
-  "src/an1me/http.js"(exports2, module2) {
-    var axios = require("axios");
-    var HEADERS = {
-      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-      "Referer": "https://an1me.to/"
-    };
-    function fetchText(url) {
-      return __async(this, null, function* () {
-        try {
-          const response = yield axios.get(url, { headers: HEADERS });
-          return response.data;
-        } catch (error) {
-          throw new Error(`Failed to fetch ${url}: ${error.message}`);
-        }
-      });
-    }
-    module2.exports = { fetchText, HEADERS };
+module.exports = {
+  getStreams: function(tmdbId, mediaType, season, episode) {
+    return Promise.resolve([{
+      name: "An1me Test",
+      title: "Working",
+      url: "https://www.w3schools.com/html/mov_bbb.mp4",
+      quality: "1080p",
+      headers: {}
+    }]);
   }
-});
-
-// src/an1me/extractor.js
-var require_extractor = __commonJS({
-  "src/an1me/extractor.js"(exports2, module2) {
-    var cheerio = require("cheerio-without-node-native");
-    var { fetchText, HEADERS } = require_http();
-    function extractStreams2(tmdbId, mediaType, season, episode) {
-      return __async(this, null, function* () {
-        const title = "naruto";
-        const url = `https://an1me.to/watch/${title}-episode-${episode}/`;
-        const html = yield fetchText(url);
-        const $ = cheerio.load(html);
-        const streams = [];
-        $("[data-embed-id]").each((i, el) => {
-          try {
-            const data = $(el).attr("data-embed-id");
-            const parts = data.split(":");
-            const serverName = atob(parts[0]);
-            const iframeHtml = atob(parts[1]);
-            const urlMatch = iframeHtml.match(/src="([^"]+)"/);
-            if (urlMatch) {
-              streams.push({
-                name: "An1me",
-                title: serverName,
-                url: urlMatch[1].replace(/&amp;/g, "&"),
-                quality: "1080p",
-                headers: HEADERS
-              });
-            }
-          } catch (e) {
-          }
-        });
-        return streams;
-      });
-    }
-    module2.exports = { extractStreams: extractStreams2 };
-  }
-});
-
-// src/an1me/index.js
-var { extractStreams } = require_extractor();
-function getStreams(tmdbId, mediaType, season, episode) {
-  return __async(this, null, function* () {
-    try {
-      return yield extractStreams(tmdbId, mediaType, season, episode);
-    } catch (error) {
-      console.error("Provider Error:", error.message);
-      return [];
-    }
-  });
-}
-module.exports = { getStreams };
+};
--- src/an1me/index.js
+++ src/an1me/index.js
@@ -1,12 +1,12 @@
-const { extractStreams } = require('./extractor.js');
-
+// index.js - The absolute minimum
 async function getStreams(tmdbId, mediaType, season, episode) {
-    try {
-        return await extractStreams(tmdbId, mediaType, season, episode);
-    } catch (error) {
-        console.error("Provider Error:", error.message);
-        return [];
-    }
+    return [{
+        name: "An1me Test",
+        title: "Working",
+        url: "https://www.w3schools.com/html/mov_bbb.mp4",
+        quality: "1080p",
+        headers: {}
+    }];
 }
 
 module.exports = { getStreams };
