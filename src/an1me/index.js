68ad587e1d952f447cb067f12f3f3b00bea8a826
Lliakos <philip_k06@yahoo.com>
2026-06-04 16:00:11 +0300

    TESTTTT

--- src/an1me/index.js
+++ src/an1me/index.js
@@ -1,14 +1,4 @@
-const { getPage } = require('./http.js');
-const { extract } = require('./extractor.js');
-
 async function getStreams(tmdbId, mediaType, season, episode) {
-  try {
-    const html = await getPage(tmdbId, mediaType, season, episode);
-    return extract(html);
-  } catch (e) {
-    console.error(e);
-    return [];
-  }
+  return [{ title: "TEST", url: "https://google.com", headers: {} }];
 }
-
 module.exports = { getStreams };
